99c1c990a95ea771da5252cb640f943cf48ab6d8
romanow-nsk <romanow@ngs.ru>
2022-11-12 00:02:04 +0700

    Конвертированный в JS

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -23,8 +23,8 @@ suspend fun main(args: Array<String>) {
     val container = document.createElement("div")
     document.body!!.appendChild(container)
     val api: RestAPIBase = RestAPIBase()
-    api.ip="217.71.138.9"
-    api.port=4569
+    //api.ip="217.71.138.9"
+    //api.port=4567
     val ss = api.keepalive("1111111")
     val userPair = api.login("9136666666","1234")
     var out = ""
